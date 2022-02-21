9e99a3e4ae41c9114ad9f915bf512a46ff6990cc
jstrombe19 <jaredstromberg@gmail.com>
2022-02-20 22:31:21 -0700

    functional data manipulation and correct calculations implemented. still need to convert strings to boolean values for elastic array.

---
+++ mod6-reading/one-d-collisions/main.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def read_data(filename):
+    # complete the read_data() function
+
+    elastic = []
+    measurements = []
+    # elastic, measurements = None
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith(','):
+                continue
+            values = line.split(',')
+            elastic.append(values[-1])
+            measurements.append(values[1:-1])
+    
+    elastic = np.array(elastic)
+    measurements = np.array(measurements, dtype=np.float64)
+    
+    
+    return elastic, measurements
+
+if __name__ == "__main__":
+    filename = "experiments_labeled.csv"
+
+    elastic, measurements = read_data(filename)
+
+    # assign to M1,M2,V1i,V2i,V1f,V2f
+    # ...
+    M1, M2, V1i, V2i, V1f, V2f = measurements.transpose()
+
+    # example use: calculate P1i
+    # ...
+    P1i = M1 * V1i
+
+    # print rounded to 4 decimals
+    print("P1i =", np.round(P1i, 4))
+
+
